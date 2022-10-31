cb475a05a46d5f3ebe43d28cf808d15d174eba16
Ignacia Silva <ignaciasilva1@gmail.com>
2022-10-31 12:40:11 -0300

    validación de fechas

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,14 +1,30 @@
-const inputNacimiento = document.querySelector("#birth")
+export function valida(input) {
+   const tipoDeInput = input.dataset.tipo;
+   if(validadores[tipoDeInput]){
+      validadores[tipoDeInput](input)
+   } 
+}
 
-inputNacimiento.addEventListener("blur", (evento) => {  //blur es cuando salga, por eso es distinto a click
-validarNacimiento(evento.target)
-});
+const validadores = {
+   nacimiento: input => validarNacimiento(input) //un input por cada item, recordar colocar en html data-tipo y el input
+}
 
 function validarNacimiento(input){
-   const fecha = new Date(input.value)
-   console.log(fecha); 
+   const fechaCliente = new Date(input.value)
+   let mensaje = ""
+   if (!mayorEdad(fechaCliente)){   //si no eres mayor de edad, entonces al hacer click en registrar, saldrá el mensaje
+      mensaje = "Debes tener al menos18 años de edad"
+   }
+
+   input.setCustomValidity(mensaje)
 }
 
 function mayorEdad(fecha){
+   const fechaActual = new Date();
+   const diferenciaFechas = new Date(
+   fecha.getUTCFullYear() + 18,
+   fecha.getUTCMonth(), 
+   fecha.getUTCDate());
 
+   return diferenciaFechas <= fechaActual; //si tienes menos de 18 años, saldrá en consola false. Si tiene más de 18 saldrá true
 }
